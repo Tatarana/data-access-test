9fecf934aa26fbb1b3171a8bb826021fdad42663
Tatarana <fernando.hreis@gmail.com>
2020-09-02 16:46:02 -0300

    Custom Attribute final

--- src/app/change-case.directive.ts
+++ src/app/change-case.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2, Input } from '@angular/core';
 
 @Directive({
   selector: '[appChangeCase]'
 })
 export class ChangeCaseDirective {
+
+  // terceira forma
+  @Input('appChangeCase') case: string;
 
   @HostListener('blur') onblur() {
     // primeira forma de fazer, mas pode não funcionar dependendo do contexto
@@ -11,7 +14,12 @@ export class ChangeCaseDirective {
     //this.elRef.nativeElement.value = this.elRef.nativeElement.value.toUpperCase();
 
     // segunra forma de fazer, parece ser a melhor.
-    let changedValue: string = this.elRef.nativeElement.value.toUpperCase();
+    //let changedValue: string = this.elRef.nativeElement.value.toUpperCase();
+    //this.renderer.setProperty(this.elRef.nativeElement, 'value', changedValue);
+
+    // terceira forma.
+    let changedValue: string = this.case === 'upper' ? this.elRef.nativeElement.value.toUpperCase()
+                                                     : this.elRef.nativeElement.value.toLowerCase();
     this.renderer.setProperty(this.elRef.nativeElement, 'value', changedValue);
   }
 
