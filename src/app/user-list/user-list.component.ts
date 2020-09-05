14335d4590fc57d85c2b08c5fa4f4650a5089988
Tatarana <fernando.hreis@gmail.com>
2020-09-05 17:17:12 -0300

    2 files modified

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from '../user-interface';
 import { UserService } from '../services/user.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HookLogger } from '../decorators/class.decorator';
+
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
+@HookLogger(['ngOnInit'])
 export class UserListComponent implements OnInit {
 
   users: IUser[];
